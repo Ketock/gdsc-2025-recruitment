bdcc58794849e8e623ab65e41b2f55f75d60d523
Ketock <137045353+Ketock@users.noreply.github.com>
2025-03-05 16:49:51 +0530

    Update main.cpp

--- Chip8/main.cpp
+++ Chip8/main.cpp
@@ -1,1 +1,15 @@
+#include "Chip8.h"
+#include <iostream>
+
+int main() {
+    Chip8 chip8;
+    chip8.LoadProgram("roms/test.ch8");
+
+    while (true) {
+        chip8.EmulateCycle();
+        // Add delay or timing logic later
+    }
+
+    return 0;
+}
 
